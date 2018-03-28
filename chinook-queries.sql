a6fddf4579e4769f1c290c3e5164cff0c7d625b3
joechesney <33613119+joechesney@users.noreply.github.com>
2018-03-28 15:33:39 -0500

    got some numbered

--- chinook-queries.sql
+++ chinook-queries.sql
@@ -1,48 +1,48 @@
--- Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
+--1 Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
 SELECT "CustomerId", "FirstName", "LastName", "Country" 
 FROM "Customer" 
 WHERE Customer.Country IS NOT "USA";
 
--- Provide a query only showing the Customers from Brazil.
+--2 Provide a query only showing the Customers from Brazil.
 SELECT Customer.* 
 FROM "Customer" 
 WHERE Customer.Country IS "Brazil";
 
--- Provide a query showing the Invoices of customers who are from Brazil. The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country.
+--3 Provide a query showing the Invoices of customers who are from Brazil. The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country.
 SELECT concat(Customer.FirstName, Customer.LastName), Invoice.InvoiceId, Invoice.InvoiceDate, Invoice.BillingCountry
 FROM Customer
 JOIN Invoice
 WHERE Customer.CustomerId = Invoice.CustomerId
 AND Customer.Country = "Brazil";
 
--- Provide a query showing only the Employees who are Sales Agents.
+--4 Provide a query showing only the Employees who are Sales Agents.
 SELECT * 
 FROM Employee e
 WHERE e.title = "Sales Support Agent";
 
--- Provide a query showing a unique list of billing countries from the Invoice table.
+--5 Provide a query showing a unique list of billing countries from the Invoice table.
 SELECT DISTINCT i.BillingCountry FROM Invoice i;
 
--- Provide a query that shows the invoices associated with each sales agent. The resultant table should include the Sales Agent's full name.
+--6 Provide a query that shows the invoices associated with each sales agent. The resultant table should include the Sales Agent's full name.
 SELECT (Customer.FirstName || ' ' || Customer.LastName) AS "Full Name", Invoice.InvoiceId, Invoice.InvoiceDate, Invoice.BillingCountry
 FROM Customer
 JOIN Invoice
 WHERE Customer.CustomerId = Invoice.CustomerId
 AND Customer.Country = "Brazil";
 
--- Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers.
+--7 Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers.
 
 
--- How many Invoices were there in 2009 and 2011? What are the respective total sales for each of those years?
--- Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37.
--- Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY
--- Provide a query that includes the track name with each invoice line item.
--- Provide a query that includes the purchased track name AND artist name with each invoice line item.
--- Provide a query that shows the # of invoices per country. HINT: GROUP BY
--- Provide a query that shows the total number of tracks in each playlist. The Playlist name should be included on the resultant table.
--- Provide a query that shows all the Tracks, but displays no IDs. The resultant table should include the Album name, Media type and Genre.
--- Provide a query that shows all Invoices but includes the # of invoice line items.
--- Provide a query that shows total sales made by each sales agent.
+--8 How many Invoices were there in 2009 and 2011? What are the respective total sales for each of those years?
+--9 Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37.
+--10 Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY
+--11 Provide a query that includes the track name with each invoice line item.
+--12 Provide a query that includes the purchased track name AND artist name with each invoice line item.
+--13 Provide a query that shows the # of invoices per country. HINT: GROUP BY
+--14 Provide a query that shows the total number of tracks in each playlist. The Playlist name should be included on the resultant table.
+--15 Provide a query that shows all the Tracks, but displays no IDs. The resultant table should include the Album name, Media type and Genre.
+--16 Provide a query that shows all Invoices but includes the # of invoice line items.
+--17 Provide a query that shows total sales made by each sales agent.
 -- Which sales agent made the most in sales in 2009?
 -- Which sales agent made the most in sales in 2010?
 -- Which sales agent made the most in sales over all?
